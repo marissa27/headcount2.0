db67530f0cdc864cbcb9d567489c5236250633b5
Marissa Reinke <reinke.marissa@gmail.com>
2017-03-14 15:14:34 -0600

    connect files and pass object data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import CardList from './CardList/cardList.js';
+
 
 class App extends Component {
   render() {
     return (
-      <div className ="welcome">Welcome To Headcount 2.0</div>
+      <div className="welcome">Welcome To Headcount 2.0
+        <CardList />
+      </div>
     );
   }
 }
