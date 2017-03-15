12dc46d0f22d585b341f562d138aed40b1064bf3
Marissa Reinke <reinke.marissa@gmail.com>
2017-03-15 15:29:41 -0600

    git problems

--- src/Card/card.js
+++ src/Card/card.js
@@ -1,27 +1,5 @@
 import React, { Component } from 'react';
-import CardList from '../CardList/cardList.js';
-import '../App.css';
 
-export default class Card extends Component  {
-  constructor() {
-    super();
 
-  }
-    render() {
-      // console.log(this.props.locData)
-      const { locData } = this.props
-      // console.log(this.props)
-      const cardData = Object.keys(locData).map((year, i) => {
-        // console.log([year])
-        return (
-          <li key={i}>{year}: {locData[year]}</li>
-        )
-      })
-        return (
-            <div className="district-card">
-              <h6>{ this.props.loc }</h6>
-              <ul>{ cardData }</ul>
-            </div>
-        );
-    }
-}
+
+export default Card;
