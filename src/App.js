0b4d2f8cd39e1a6ed93110f2adc40b1358f33f29
Staci McWilliams <stacinicole86@gmail.com>
2017-03-15 15:24:03 -0600

    refactor app and set state for data

--- src/App.js
+++ src/App.js
@@ -2,15 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 import CardList from './CardList/cardList.js';
 import Search from './Search/search.js';
+import DistrictRepository from './helper.js';
+import kinderData from '../data/kindergartners_in_full_day_program.js';
 
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      dr: {},
+    }
+  }
+
+  componentWillMount() {
+    this.setState({
+      dr: new DistrictRepository(kinderData)
+    })
+  }
+
   render() {
+
+      console.log(this.state.dr.findByName('colorado'))
+    // debugger
     return (
       <div>
         <Search />
         <h1 className="welcome">Weclome to Headcount</h1>
-        <CardList />
+        <CardList data={this.state.dr.data} />
       </div>
     );
   }
